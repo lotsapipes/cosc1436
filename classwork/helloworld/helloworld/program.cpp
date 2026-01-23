aa5f470ca829a3da285833e42c1fe4db0082b73c
Robert Pace <robert.pace@my.tccd.edu>
2026-01-22 21:36:53 -0600

    includes some notes to self, but it's just helloworld.

---
+++ classwork/helloworld/helloworld/program.cpp
@@ -0,0 +1,14 @@
+
+//some hello world lore i missed somewhere
+//a program for printing hello world:
+
+
+//no idea what this thing below is:
+#include <iostream>
+
+//this thingy should be called a block
+void main()
+{
+    std::cout << "Hello World";
+    int x = 10;
+}
