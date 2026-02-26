e004bf61bde7cbedc330acea0edd691688730272
lotsapipes <robert.pace@my.tccd.edu>
2026-02-26 23:51:30 +0000

    pretty much done here

--- labs/Lab 1.cpp
+++ labs/Lab 1.cpp
@@ -1,10 +1,14 @@
 /*
 * Robert Pace
-* COSC 1436
-* Lab 1 alskdfjsdl
+* COSC 1436-21005
+* Lab 1
+* Spring 2026
 */
 
+//git push
+//git commit -m "required message goes here"
 
+//git add .
 
 #include <iostream>
 #include <iomanip>
@@ -13,11 +17,13 @@
 
 int main() {
 
+std::cout << "Robert Pace\n";
+std::cout << "COSC 1436-21005\n";
+std::cout << "Spring 2026\n";
+std::cout << "Lab 1: Student Grades and Weights\n\n\n";
+
 std::string studentName;
-//rebuild struct or something right here
-//because we're not allowed to use vectors yet
-//struct (or something) labs
-//struct exams
+
 struct labGrades {
     double labOne;
     double labTwo;
@@ -44,7 +50,7 @@ std::getline(std::cin, studentName);
 //also, note below, the part we got stuck on fits right there
 //into the first slot thingy of the variable
 
-std::cout << "/nEnter 4 lab grades (1-100, separated by enter/spaces):\n";
+std::cout << "Enter 4 Lab grades below (1-100, then press Enter after inputting each one):\n";
 std::cout << "Lab 1: ";
 std::cin >> labs.labOne;
 std::cout << "Lab 2: ";
@@ -54,7 +60,7 @@ std::cin >> labs.labThree;
 std::cout << "Lab 4: ";
 std::cin >> labs.labFour;
 
-std::cout << "Next, enter the 3 exam grades (1-100, separated by enter/spaces):\n";
+std::cout << "Next, enter the 3 Exam grades below (1-100, then press Enter after inputting each one):\n";
 std::cout << "Exam 1: ";
 std::cin >> exams.examOne;
 std::cout << "Exam 2: ";
@@ -65,81 +71,66 @@ std::cin >> exams.examThree;
 std::cout << "Enter the Participation grade: ";
 std::cin >> participation;
 
-std::cout << "Enter the final exam grade: ";
+std::cout << "Enter the Final Exam grade: ";
 std::cin >> finalExam;
 
-std::cout << "\n------------------------------------\n";
+std::cout << "\n-----------------------------------\n";
 std::cout << "Final Results\n";
-std::cout << "-------------------------------------";
-std::cout << "Student: " << studentName << "\n\n";
+std::cout << "-----------------------------------\n";
+std::cout << "Student Name: " << studentName << "\n\n";
 
-std::cout << "Lab Grades:\n";
+std::cout << "Here's your Lab grades, " << studentName << ":\n";
 std::cout << "Lab 1: " << labs.labOne << std::endl; //swap with \n if this don't work
 std::cout << "Lab 2: " << labs.labTwo << std::endl;
 std::cout << "Lab 3: " << labs.labThree << std::endl;
 std::cout << "Lab 4: " << labs.labFour << std::endl;
+std::cout << std::endl;
 
-std::cout << "Exam Grades:\n";
+std::cout << "Here's your Exam grades, " << studentName << ":\n";
 std::cout << "Exam 1: " << exams.examOne << std::endl; //swap with \n if this don't work
 std::cout << "Exam 2: " << exams.examTwo << std::endl; //swap with \n if this don't work
 std::cout << "Exam 3: " << exams.examThree << std::endl; //swap with \n if this don't work
+std::cout << std::endl;
 
-std::cout << "\nOther Grades: \n";
-std::cout << "  Participation: " << participation << "\n";
-std::cout << "  Final Exam: " << "\n";
+std::cout << "Your other grades are below, " << studentName << ":\n";
+std::cout << "Participation: " << participation << "\n";
+std::cout << "Final Exam: " << finalExam << "\n";
 
 
 //can't remember if we're allowed to use for loops to do the math here or not
 //double check later, if not, just format this a little better
 //so it don't look so bad
 
-double labTotal = labs.labTwo + labs.labThree
- + labs.labThree +labs.labFour;
+double labTotal = labs.labOne + labs.labTwo + labs.labThree +labs.labFour;
 double labAverage = labTotal / 4.0;
 
-double examTotal = exams.examOne + exams.examTwo
- + exams.examThree;
- double examAverage = examTotal / 3.0;
+double examTotal = exams.examOne + exams.examTwo + exams.examThree;
+double examAverage = examTotal / 3.0;
 
 
  //IMPORTANT: double check these weights against the stories or whatever
- double weightedFinal = (labAverage * 0.25) +
-                        (examAverage * .50) +
-                        (participation * 0.10) +
-                        (finalExam * 0.15);
+ double weightedFinal = (labAverage * 0.65) +
+                        (examAverage * 0.20) +
+                        (participation * 0.05) +
+                        (finalExam * 0.10);
 
 
 //note, there's a typo above somewhere where i forgot to type the %
 //marks, add them later
 std::cout << std::fixed << std::setprecision(2);
-std::cout << "Grade Calculations:\n";
-std::cout << "-------------------\n";
-std::cout << "Lab Average (25% weight): " << labAverage << "%\n";
-std::cout << "Exam Average (50% weight): " << examAverage << "%\n";
-std::cout << "Participation (10% weight): " << participation << "%\n";
-std::cout << "Final Exam (15% weight): " << finalExam << "%\n";
-std::cout << "----------------------------------------------\n";
-std::cout << "Final Course Grade :" << weightedFinal << "%\n;";
+std::cout << "\nHere's your final Class Grade calculations, " << studentName << ":\n";
+std::cout << "-----------------------------------\n";
+std::cout << "Lab Average (65% weight): " << labAverage << "%\n";
+std::cout << "Exam Average (20% weight): " << examAverage << "%\n";
+std::cout << "Participation (5% weight): " << participation << "%\n";
+std::cout << "Final Exam (10% weight): " << finalExam << "%\n";
+std::cout << "-----------------------------------\n";
+std::cout << "Final Course Grade :" << weightedFinal << "%\n";
+std::cout << std::endl;
+std::cout << std::endl;
 
 //don't forget to swap out the int main above for a void main
 //as well as removing the return zero below
 
 return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
