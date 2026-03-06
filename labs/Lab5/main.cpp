ae3fe50072352abe8a482b98b4ee8829299b699b
Robert Pace <robert.pace@my.tccd.edu>
2026-03-05 19:45:44 -0600

    creating a placeholder so we can edit this later using VS code or Codespaces

---
+++ labs/Lab5/main.cpp
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+
+
+//this is a placeholder to ensure that the configuration files (i forget what they're called right now)
+//are created properly using Visual Studio
+//now we can edit this thing using any tool we want.
