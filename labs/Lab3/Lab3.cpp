d0ad9a6110f75784c1e68071aa3b10e9dcd929ec
lotsapipes <robert.pace@my.tccd.edu>
2026-04-08 01:12:30 +0000

    polishing homework / testing on Windows VM in class

--- labs/Lab3/Lab3.cpp
+++ labs/Lab3/Lab3.cpp
@@ -1,6 +1,3 @@
-//this is a placeholder to ensure that the configuration files (i forget what they're called right now)
-//are created properly using Visual Studio
-//now we can edit this thing using any tool we want.
 
 // Robert Pace
 // Lab 3: Free Fall Distance & Velocity Calculator
@@ -8,67 +5,53 @@
 // COSC-1436-21005
 
 
-
-
 // Things we need to fix:
 
-// Don't forget to check this in class!
-
-// The Unit Measurement on the user's table isn't aligning properly
-// Tidying up the stuff we're not allowed to use
-// Removing extra comments we have in here for studying
-
 // Go through the canvas module page and format this thing exactly as described
-// Don't forget to pay attention to how this stuff is supposed to be named, we may need the Windows VM for this
-// mostly done for now though
-
-
+// Project name must be FallingDistance
+// Solution name must be called Lab3
 
 #include <iostream>
 #include <iomanip>
 #include <string>
 #include <limits>
-//#include <cctype>  // it's a converter we're not supposed to be using
-//using namespace std; //also not allowed to use, may as well memorize where the std:: things go
 
-// Constants
-// gonna get used to reading/writing these in all caps even tho it looks weird:
+// Constants (usually accepted in CAPS form)
 const double GRAVITY = 9.8;  // meters per second squared
 const double TERMINAL_VELOCITY = 90.0;  // meters per second
 const double METERS_TO_FEET = 3.28084;
 
-// these are called function prototypes, we can put these on top and define them later
-// something about how we want main() to know they exist, more on that later
+// Function Prototypes (Compiler needs them in advance)
 int getFallingTime();
 char getUnits();
-// the stuff labeled in the parentheses here are telling the function what it Returns
 double calculateDistance(int seconds);
 double calculateVelocity(int seconds);
 double convertToFeet(double meters);
 void displayTable(int maxTime, char unit);
+void displayProgramInfo();
 
-
-//main is supposed to be as streamlined as possible
-//main can be thought of as the 'front-end' for our use cases for now (maybe later too)
+// Main Function (Entry point, in our case it's user front-end, later on it can get even smaller)
 int main() {
-    // Program information
-    // Sometimes it's more readable just to type the whole thing out
-    std::cout << "===========================================" << std::endl;
+    
+    displayProgramInfo();
+
+    // Typing out the table in easy, readable form for this one:
+    std::cout << "===============================================" << std::endl;
     std::cout << "     Free Fall Distance & Velocity Calculator" << std::endl;
-    std::cout << "===========================================" << std::endl;
+    std::cout << "===============================================" << std::endl;
     std::cout << "This program calculates the distance fallen" << std::endl;
     std::cout << "and velocity reached by an object in free fall." << std::endl;
-    std::cout << "Gravity is set to 9.8 m/s²" << std::endl;
+    std::cout << "Gravity is set to 9.8 m/s^2" << std::endl;
     std::cout << "Terminal velocity is limited to 90 m/s." << std::endl;
-    std::cout << "===========================================" << std::endl << std::endl;
+    std::cout << "===============================================" << std::endl << std::endl;
     
-    // Story 2: Get falling time with validation
+    // Story 2: Get falling time from the user
     int fallingTime = getFallingTime();
     
-    // Story 5: Get units preference with validation
+    // Story 5: Get units preference from the user
     char units = getUnits();
     
-    // Story 4 & 6 & 7: Display table with all data
+    // Story 4 & 6 & 7: Creation of table, updating table, final table
     displayTable(fallingTime, units);
     
     return 0;
@@ -82,29 +65,25 @@ int getFallingTime() {
     do {
         std::cout << "Please enter the number of seconds? ";
 
-        // below is the same input buffer fix we keep running into while building these things
-        // the part we need to memorize is the first line, but this is the 2nd time i've gotten stuck with these problems
-        // i think i used it in lab 2 for a boolean check or something
-        // also don't forget to maybe add an explanation for that this time
-
-        //it's this line we keep seeing
+        // Below is likely code from outside of class (I keep seeing it while studying)
+        // It basically just says "if 'time' did not cin properly"
         if (!(std::cin >> time)) {
-        // extraction failed (user typed a letter)
+        // Below may be an input buffer clearing method from outside of class
+        // It seems to work fairly well (I also keep seeing this one)
         std::cin.clear(); 
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         std::cout << "error: Try entering a number between 1-60.\n";
-        continue; // restart loop
-    }
-        //end of stuff we added    
-
-
+        continue; // restarts current loop (had to study why this is a bit)
+    }   
         // Validate input is between 1 and 60
         if (time >= 1 && time <= 60) {
             valid = true;
         } else {
             std::cout << "error: Try entering a time must be between 1 and 60 seconds: " << std::endl;
         }
-        // don't forget this easy way to say that something isn't valid:
+        
+      // Loops all of the above until the 'valid' Boolean checking for proper input
+      // changes to 'True'  
     } while (!valid);
     
     return time;
@@ -119,15 +98,11 @@ char getUnits() {
         std::cout << "Do you want the results in meters or feet? ";
         std::cin >> units;
         
-        // Convert to lowercase for case-insensitive comparison
-        // units = tolower(units); //this part is the thing we prolly gotta rework
-        
-        // i just switched back to writing out a longer form and run OR checks on everything
         if (units == 'm' || units == 'f'
             || units == 'M' || units == 'F') {
             valid = true;
         } else {
-            std::cout << "ERROR: Please enter 'm' for meters or 'f' for feet." << std::endl;
+            std::cout << "Error: Please enter 'm' for meters or 'f' for feet." << std::endl;
         }
     } while (!valid);
     
@@ -157,6 +132,16 @@ double convertToFeet(double meters) {
     return meters * METERS_TO_FEET;
 }
 
+// Simple Student Name and Header
+void displayProgramInfo() {
+    std::cout << "Robert Pace" << std::endl;
+    std::cout << "Lab 3: Free Fall Distance & Velocity Calculator" << std::endl;    
+    std::cout << "Spring 2026" << std::endl;
+    std::cout << "COSC-1436-21005" << std::endl;
+    std::cout << "\n"; 
+}
+
+
 // Story 4, 6, & 7: Display table with all data in selected units
 void displayTable(int maxTime, char unit) {
     std::cout << "\n\n";
@@ -166,7 +151,7 @@ void displayTable(int maxTime, char unit) {
     std::cout << std::left << std::setw(15) << "Distance";
     std::cout << "Velocity" << std::endl;
     
-    // Display separator line
+    // Display separator line (using condensed format)
     std::cout << std::string(40, '=') << std::endl;
     
     // Display data for each second from 1 to maxTime
@@ -193,20 +178,11 @@ void displayTable(int maxTime, char unit) {
             velocityUnit = "ft/s";
         }
         
-        // Display the row
+        // Display the row (as best we can)
         std::cout << std::left << std::setw(12) << t;
         std::cout << std::left << std::fixed << std::setprecision(1) << distance << " " << distanceUnit;
-        // we had to remove a setw(12) here because it wasn't fitting right for some reason
-        // here's a hacky way to do it for now:
-        //std::cout << "        ";
-        // oh well this works below:
         std::cout << std::setw(8) << " ";
         std::cout << std::left << std::fixed << std::setprecision(1) << velocity << " " << velocityUnit << std::endl;
-        // we think at some point we're going to run into:
-        // std::ostringstream
-        // std::format
-        // full blown libraries like fmt, tabulate
-        // haven't finished reading about these yet
     }
     
     std::cout << std::endl;
