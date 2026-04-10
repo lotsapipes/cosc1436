f1d68b4cb1094974aa5bdab9309b8f4d2cb9f2bf
Robert Pace <robert.pace@my.tccd.edu>
2026-04-09 19:32:07 -0500

    trying to keep up in class

--- classwork/MovieLibrary/MovieLibrary/program.cpp
+++ classwork/MovieLibrary/MovieLibrary/program.cpp
@@ -9,6 +9,151 @@
 #include <iomanip>
 #include <limits>
 
+// moving notes to the bottom so we can see what we're doing (fell back to continuing notes at the bottom anyways, trying too hard to refactor and catch up)
+// deleting some notes
+// it looks like adding an array of 100 movies is required
+// beginning typing and following along using near matches for variable names and such
+
+//structs go above function prototypes
+struct Movie{
+    int id;
+    std::string title;
+    std::string genres;
+    int runLength = 0;
+    int releaseYear = 0;
+    double userRating = 0;
+    char isClassic = false;
+    std::string description;
+};
+
+// function prototype attempts
+int FindMovieById(Movie movies[], int size, int id);
+void deleteMovie(Movie& movie);
+void deleteMovies(Movie movies[], int size);
+void viewMovie(const Movie& movie);
+
+int main()
+{
+        std::cout << "Main Menu" << std::endl;
+        std::cout << "-----------------" << std::endl;
+        std::cout << "a) Add Movie" << std::endl;
+        std::cout << "e) Edit Movie" << std::endl;
+        std::cout << "d) Delete Movie" << std::endl;
+        std::cout << "v) View Movie" << std::endl;
+        std::cout << "?) Help" << std::endl;
+        std::cout << "Pick one of the letters from the menu to continue: ";
+
+        char choice;
+        std::cin >> choice;
+   
+    if (choice == 'a' || choice == 'A') {
+        std::cout << "You chose to add a movie!" << std::endl;
+
+    }
+    else if (choice == 'e' || choice == 'E') {
+        std::cout << "You chose to edit a movie!" << std::endl;
+    }
+    else if (choice == 'd' || choice == 'D') {
+        std::cout << "You chose to delete a movie!" << std::endl;
+    }
+    else if (choice == 'v' || choice == 'V') {
+        std::cout << "You chose to view a movie!" << std::endl;
+    }
+    else if (choice == '?') {
+        std::cout << "Help menu coming soon!" << std::endl;
+    }
+    while (choice != 'a' && choice != 'A' && choice != 'e' && choice != 'E' && choice != 'd' && choice != 'D' && choice != 'v' && choice != 'V' && choice != '?') {
+        std::cout << "Invalid choice. Please enter a valid option." << std::endl;
+        std::cin.clear(); // this is half of the input buffer clear we've learned
+        std::cin >> choice;
+    }
+
+   Movie movie;
+
+   Movie movies[100];
+
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Enter the movie title: ";
+    std::getline(std::cin, movie.title);
+    std::cout << "Enter description: ";
+    std::getline(std::cin, movie.description);
+    std::cout << "Enter the movie genres: ";
+    std::getline(std::cin, movie.genres);
+    std::cout << "Enter the movie run length (in minutes): ";
+    std::cin >> movie.runLength;
+
+    if (movie.runLength < 0) {
+        std::cout << "Invalid run length. Please enter a positive integer: ";
+        std::cin >> movie.runLength;
+    }
+
+    std::cin.clear();
+    std::cout << "Enter the movie release year (1900 - 2100): ";
+    std::cin >> movie.releaseYear;
+
+    if (movie.releaseYear < 1900 || movie.releaseYear > 2100) {
+        std::cout << "Invalid release year. Please enter a year between 1900 and 2100: ";
+        movie.releaseYear = 0;
+        std::cin >> movie.releaseYear;
+    }
+
+    std::cout << "Enter the movie user rating (0.0 - 5.0): ";
+    std::cin >> movie.userRating;
+    std::cout << "Is this movie a classic? (Y/N): ";
+    std::cin >> movie.isClassic;
+    if (movie.isClassic == 'Y' || movie.isClassic == 'y') {
+        movie.isClassic = true;
+    }
+    if (movie.isClassic == 'N' || movie.isClassic == 'n') {
+        movie.isClassic = false;
+
+        std::cout << "Movie Title: " << movie.title << std::endl;
+        std::cout << "Genres: " << movie.genres << std::endl;
+        std::cout << "Run Length: " << movie.runLength << " minutes" << std::endl;
+        std::cout << "Release Year: " << movie.releaseYear << std::endl;
+        std::cout << "User Rating: " << std::fixed << std::setprecision(1) << movie.userRating << " / 5.0" << std::endl;
+        std::cout << "Is Classic: " << (movie.isClassic ? "Yes" : "No") << std::endl;
+
+        std::cout << "Description: " << movie.description << std::endl;
+    }
+    return 0;
+
+
+
+
+
+
+
+
+
+
+
+}
+
+/*
+4-7-26
+learing about how arrays can't be called on by functions
+target[] is syntax we need do know
+bounds checking has been brought up many, many times in class
+
+for (int index **************
+target[index] = source[index] // this is being used in some code
+
+spent some time discussing all the things we can't do with arrays (at this point, it was mentioned that in Fundamentals 3 they'll go over the alternatives that we have
+we can't display arrays, we'll have to build one on our own it sounds like
+
+bool compareArray(checking left and right arrays to see if they're equal)
+
+
+
+*/
+
+
+
+
+
+
+
 //barely awake, just going to continue stacking notes on here and skipping most of the coding for now
 //std::setw(width) ; i don't remember why we're allowed to type 'width' here
 //we're noting the important differences between PARAMETERS and ARGUMENTS, pay attention because it may be on the quiz
@@ -76,138 +221,18 @@
 //also, don't forget it throws compile errors if you declare it and then don't define or call/implement it
 
 //LOOKS LIKE WE'RE GOING TO BE PUTTING FUNCTIONS ABOVE MAIN INSTEAD OF AT THE BOTTOM
-void DisplayLine()
-{
-    std::cout << "---------------------" << std::endl;
-}
 
-void setErrorColor() 
-{
-    std::cout << "\033[31m";
-};
 
-void resetColor() 
-{
-    std::cout << "\033[0m";   // Resets to default
-}
-
-struct Movie
     //don't forget, a Struct is a user-defined data type that can hold multiple values of different types. 
     // It's a way to group related data together. In this case, we have a Movie struct that has several fields: 
     // title, genres, runLength, releaseYear, userRating, isClassic, and description. 
     // Each field has a specific type (string, int, double, bool) that defines what kind of data it can hold.
 
-{
-    std::string title;
-    std::string genres;
-
-    int runLength = 0;
-    int releaseYear = 0;
-    double userRating = 0;
-    char isClassic = false;
-    std::string description;
-
-};
-
-int main()
-{
-    //THIS IS HOW WE CALL THE FUNCTION WE JUST BUILT ABOVE:
-    DisplayLine();
-    setErrorColor();
-
-    //we also spent time moving variables around including "promoting" some of these variables to a spot outside of 2 or more blocks so multiple
-    //blocks could use them.
-
-        std::cout << "Main Menu" << std::endl;
-        std::cout << "-----------------" << std::endl;
-        std::cout << "a) Add Movie" << std::endl;
-        std::cout << "e) Edit Movie" << std::endl;
-        std::cout << "d) Delete Movie" << std::endl;
-        std::cout << "v) View Movie" << std::endl;
-        std::cout << "?) Help" << std::endl;
-        std::cout << "Pick one of the letters from the menu to continue: ";
-
-        char choice;
-        std::cin >> choice;
-
         //missed some parts talking about something called FALLTHROUGH (which sometimes we actually want for some reason)
         //the fix is apparently to use BREAKS
         //you can stake multiple 'lines' into one as shown below with the breaks (i skipped some)
-   
-    if (choice == 'a' || choice == 'A') {
-        std::cout << "You chose to add a movie!" << std::endl;
-    }
-    else if (choice == 'e' || choice == 'E') {
-        std::cout << "You chose to edit a movie!" << std::endl;
-    }
-    else if (choice == 'd' || choice == 'D') {
-        std::cout << "You chose to delete a movie!" << std::endl;
-    }
-    else if (choice == 'v' || choice == 'V') {
-        std::cout << "You chose to view a movie!" << std::endl;
-    }
-    else if (choice == '?') {
-        std::cout << "Help menu coming soon!" << std::endl;
-    }
-    while (choice != 'a' && choice != 'A' && choice != 'e' && choice != 'E' && choice != 'd' && choice != 'D' && choice != 'v' && choice != 'V' && choice != '?') {
-        std::cout << "Invalid choice. Please enter a valid option." << std::endl;
-        std::cin.clear(); // this clears the error state of the input stream, hopefully, check later
-        std::cin >> choice;
-    }
 
-    Movie movie;
-   // Below is where the array of 100 movies will go
-   // Movie movie[100];
-   Movie movie[100];
-   
-    //BELOW IS FINALLY THE LINE THAT FIXED THE INPUT BUFFER
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::cout << "Enter the movie title: ";
-    std::getline(std::cin, movie.title);
-    std::cout << "Enter description: ";
-    std::getline(std::cin, movie.description);
-    std::cout << "Enter the movie genres: ";
-    std::getline(std::cin, movie.genres);
-    std::cout << "Enter the movie run length (in minutes): ";
-    std::cin >> movie.runLength;
-    //TODO: validate the run length to make sure it's a positive integer (maybe we can use a loop to keep asking until we get a valid run length)
-
-    //NOTE: bracket placement on these ones below? ah, found it, it's called a BLOCK STATEMENT, notes below:
-
-    if (movie.runLength < 0) {
-        std::cout << "Invalid run length. Please enter a positive integer: ";
-        std::cin >> movie.runLength;
-    }
-
-    //block statement is a group of statements that are enclosed in curly braces {}. 
-    // They are used to define the scope of variables and to group related statements together. 
-    // In this case, we have an if statement that checks if the run length is less than 0. 
-    // If it is, we print an error message and ask the user to enter a valid run length. 
-    // The block of code inside the if statement will only be executed if the condition is true.
-
-    //clearing the input buffer below like this:
-    std::cin.clear();
-
-    std::cout << "Enter the movie release year (1900 - 2100): ";
-    std::cin >> movie.releaseYear;
-
-    //TODO: validate the release year to make sure it's between 1900 and 2100 (maybe we can use a loop to keep asking until we get a valid release year)
-
-    // these || are called logical OR operators, they are used to combine two conditions and return true if either condition is true.
-    // technically though, at this point in the class, we'll be using two separate if statements:
-
-    //fixed the stuff below by changing from an "IF" to a "WHILE"
-    if (movie.releaseYear < 1900 || movie.releaseYear > 2100) {
-        std::cout << "Invalid release year. Please enter a year between 1900 and 2100: ";
-        movie.releaseYear = 0;
-        std::cin >> movie.releaseYear;
-         // this clears the error state of the input stream
-        //interestingly, if you put in a char instead of a number, it infinitely loops, this is because when you enter a char, 
-        // it fails to read it as an integer and leaves the input stream in a failed state.
-        // To fix this, we can clear the input stream and ignore the invalid input before asking for the release year again.
-    }
-
-     //spent some time going over && and || operators
+//spent some time going over && and || operators
      //short circuit evaluation is a concept in programming where the evaluation of a logical expression is instant on the first condition A || B
      // it's default.
      //alternatively to short circuit evaluation, we can use BITWISE operators & and |
@@ -226,61 +251,18 @@ int main()
      //if else statements are more PERFORMANT than multiple if statements because they only evaluate one condition, 
      // whereas multiple if statements will evaluate all conditions even if one is already true.
      //below we'll rewrite the /**/ commented out code using if, else if, else statements which will cover everything including the invalid input case.
-
-
-
-
-
-    std::cout << "Enter the movie user rating (0.0 - 5.0): ";
-    std::cin >> movie.userRating;
-    std::cout << "Is this movie a classic? (Y/N): ";
-    std::cin >> movie.isClassic;
-    if (movie.isClassic == 'Y' || movie.isClassic == 'y') {
-        movie.isClassic = true;
-    }
-    if (movie.isClassic == 'N' || movie.isClassic == 'n') {
-        movie.isClassic = false;
-
-
-
-
-
-    //NOTE: below is where i got lost in class because i had to fix something newbie
-    // i think all we need to do is make sure the if else thingy is working
-
-
-
-
-        std::cout << "Movie Title: " << movie.title << std::endl;
-        std::cout << "Genres: " << movie.genres << std::endl;
-        std::cout << "Run Length: " << movie.runLength << " minutes" << std::endl;
-        std::cout << "Release Year: " << movie.releaseYear << std::endl;
-        std::cout << "User Rating: " << std::fixed << std::setprecision(1) << movie.userRating << " / 5.0" << std::endl;
+     // 
         // the question mark below is a ternary operator, it's a shorthand way of writing an if-else statement. 
         // It takes three operands: a condition, a value if the condition is true, and a value if the condition is false. 
         // In this case, we're checking if movie.isClassic is true, if it is, we return "Yes", otherwise we return "No".
         //NOTE: i actually have no idea why this works.
         //actually, it's because the first slot is the true slot (where yes is) and the second slot is the false slot (where no is),
         // so if movie.isClassic is true, it will return "Yes", otherwise it will return "No".
-        std::cout << "Is Classic: " << (movie.isClassic ? "Yes" : "No") << std::endl;
-        //it's called the Conditional Operator and it's popular
-
-
-
-        std::cout << "Description: " << movie.description << std::endl;
-
-
-
-    }
-    //this is a nested if method of doing things
+//this is a nested if method of doing things
     // this is frowned up tho
         //NOTE: this thing's only checking for errors once, also have no idea how to fix it right now.
 
-
-
-
         //----------------------------------------------------------------------------------------------------------------------------------------------------
-
 
         //relational operators are used to compare two values and return a boolean result (true or false)
         // it does this using the operators: ==, !=, <, >, <=, >=
@@ -312,6 +294,18 @@ int main()
         //int result = _strcmpi("Bob", "bob"); //
         //isSingularName = _strcmpi(firstName.c_str(), lastName.c_str()) == 0; //
 
-    return 0;
-
+/*void DisplayLine()
+{
+    std::cout << "---------------------" << std::endl;
 }
+
+void setErrorColor()
+{
+    std::cout << "\033[31m";
+};
+
+void resetColor()
+{
+    std::cout << "\033[0m";   // Resets to default
+}
+*/
